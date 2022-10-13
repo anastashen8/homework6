9e7ebb20dc82879f114daf121ccae7abcaa94610
Anastasiya <anastashen8@gmail.com>
2022-10-13 18:47:41 +0300

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Print(int[] arrey)
+{    
+    for (int i = 0; i < arrey.Length; i++) 
+    {
+        Console.Write(arrey[i] + ",");
+    }
+}
+int [] array = {1, 2, 5, 7, 19};
+Print (array);
+Console.WriteLine();
+int [] array2 = {6, 1, 33};
+Print (array2);
